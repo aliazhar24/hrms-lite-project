5a68c44562efbe622721cd19877750da7ef6f411
Ali Azhar <aliazhar@Alis-MacBook-Air.local>
2026-03-15 00:35:30 +0530

    Fix CORS for deployed frontend

--- backend/main.py
+++ backend/main.py
@@ -8,16 +8,17 @@ app = FastAPI()
 frontend_url = os.getenv("FRONTEND_URL")
 
 origins = [
-    "http://localhost:5173",   # local development
-    "https://hrms-lite-project-ebon.vercel.app"             # production frontend
+   
+    frontend_url             
 ]
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_origins=["*"]
+
     
 )
 
